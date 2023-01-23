de928f6989b36577596fb224adf23faa301b6d1d
vikram phatangare <vikramgp178@gmail.com>
2023-01-23 11:49:52 +0530

    repo change
    Signed-off-by: vikram phatangare <vikramgp178@gmail.com>

--- Scripts/Demo/TC02_Manual_Mode/Script1673446477820.groovy
+++ Scripts/Demo/TC02_Manual_Mode/Script1673446477820.groovy
@@ -27,5 +27,5 @@ WebUI.setEncryptedText(findTestObject('02_Orange_HRM/Page_OrangeHRM/input_Passwo
 
 WebUI.click(findTestObject('02_Orange_HRM/Page_OrangeHRM/button_Login'))
 
-WebUI.verifyTextPresent('Dashboards', false)
+WebUI.verifyTextPresent('Dashboard', false)
 
