847ac89cbb2fc015ba0fda90167054f41ac17d4f
vikram phatangare <vikramgp178@gmail.com>
2023-01-23 11:41:14 +0530

    spell change

--- Scripts/Demo/TC02_Manual_Mode/Script1673446477820.groovy
+++ Scripts/Demo/TC02_Manual_Mode/Script1673446477820.groovy
@@ -27,5 +27,5 @@ WebUI.setEncryptedText(findTestObject('02_Orange_HRM/Page_OrangeHRM/input_Passwo
 
 WebUI.click(findTestObject('02_Orange_HRM/Page_OrangeHRM/button_Login'))
 
-WebUI.verifyTextPresent('Dashboard', false)
+WebUI.verifyTextPresent('Dashboards', false)
 
